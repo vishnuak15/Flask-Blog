961c5c9ddf2562c8d3e277e66bd896009f15e33c
Vishnu Mohandas <vishnuak15@gmail.com>
2021-08-01 22:46:29 +0530

    increased the no of posts in home

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -11,7 +11,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/home")
 def index():
     page = request.args.get('page',1,type=int)
-    post = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=1)
+    post = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=3)
     # post = Post.query.all()
     return render_template('index.html', posts=post)
 
